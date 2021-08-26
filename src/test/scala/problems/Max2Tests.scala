3e39aba01bb190da12ee788ffbd85faff9835b31
Seon Ha <seonha@unist.ac.kr>
2021-08-26 18:32:11 +0900

    Max2Test

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -8,15 +8,15 @@ import chisel3.iotesters.PeekPokeTester
 // Implement test with PeekPokeTester
 //
 class Max2Tests(c: Max2) extends PeekPokeTester(c) {
-  for (i <- 0 until 10) {
+  for (i <- 1 until 10) {
 
     // Implement below ----------
-
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+    val in0 = rnd.nextInt(10)
+    val in1 = rnd.nextInt(10)
+    poke(c.io.in0, in0)
+    poke(c.io.in1, in1)
     step(1)
-    expect(c.io.out, 1)
-
-    // Implement above ----------
+    expect(c.io.out, if(in0 > in1) in0 else in1)
+   // Implement above ----------
   }
 }
